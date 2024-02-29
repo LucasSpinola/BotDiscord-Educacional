5da70507ff3ce711a5215d448869bc5f8d831bda
Lucas Spinola <108233562+LucasSpinola@users.noreply.github.com>
2024-02-28 22:01:32 -0300

    v1-att

--- settings.py
+++ settings.py
@@ -1,66 +1,64 @@
-import os
-import logging
-from logging.config import dictConfig
 from dotenv import load_dotenv
-import discord
+import os
+from logging.config import dictConfig
+import logging
 import pathlib
-
+import discord
 
 load_dotenv()
 
-TOKEN_BOT = os.getenv('TOKEN')
+DISCORD_TOKEN = os.getenv("TOKEN")
 
-BASE_DIR = pathlib.Path(__file__).parent
+API_BOT = os.getenv("API_URL")
 
 GUILDS_ID = discord.Object(id=int(os.getenv("GUILD")))
 
-CMDS_DIR = BASE_DIR / 'cmds'
+BASE_DIR = pathlib.Path(__file__).parent
 
-COG_DIR = BASE_DIR / 'cogs'
-
-LOGGING_CONFIG = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s'
+CMDS_DIR = BASE_DIR / "cmds"
+COGS_DIR = BASE_DIR / "cogs"
+LOGGIN_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": " %(levelname)s - %(asctime)s - %(module)-15s - %(message)s"
         },
-        'standard': {
-            'format': '%(levelname)-10s - %(name)-15s : %(message)s'
-            
-        },
-        
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'standard'
-        },
-        'console2': {
-            'level': 'WARNING',
-            'class': 'logging.StreamHandler',
-            'formatter': 'standard'
-        },
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'filename': 'logs/info.log',
-            'mode': 'w',
-            'formatter': 'verbose'
+        "standart": {
+            "format": " %(levelname)s - %(name)-15s - %(message)s"
         },
     },
-    'loggers': {
-        'bot': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': False
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "standart"
         },
-        'discord': {
-            'handlers': ['console2', 'file'],
-            'level': 'INFO',
-            'propagate': False
+        "console2": {
+            "level": "WARNING",
+            "class": "logging.StreamHandler",
+            "formatter": "standart"
         },
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "Logs/infos.log",
+            "mode": "w",
+            "formatter": "verbose"
+        }
     },
-    
+    "loggers": {
+        "bot": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False
+        },
+        "discord": {
+            "handlers": ["console2", "file"],
+            "level": "INFO",
+            "propagate": False
+        }
+    }
 }
+
+dictConfig(LOGGIN_CONFIG)
